1f54aedd41a83327ae5f140c121e6c6f9d4ba36f
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-06-25 19:28:37 +0500

    node modules

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
-const path = require("path");
+// const fs = require("fs");
 
-console.log(path.normalize("desktio//\\//node///index"));
+// fs.readdir("./", function (err, files) {
+//   if (err) console.log(err);
+//   else console.log(files);
+// });
+
+// let rF = fs.readFileSync("./index.js", "utf8");
+
+// console.log(rF);
+
+// fs.readFile("./index.js", "utf8", (err, file) => {
+//   if (err) console.log(err);
+//   else console.log(file);
+// });
+
+// const writeF = fs.writeFileSync("./newJsFile.js", "const name='Ali'");
+
+// console.log(writeF);
+
+// fs.writeFile("./newFile.js", "Hellow", "utf8", function (err, data) {
+//   if (err) console.log(err);
+//   else console.log(data);
+// });
+
+// let renSync = fs.renameSync();
+
+// fs.rename("./newFile.txt", "code.js", (oldPath, newPath) => {
+//   if (oldPath) console.log(oldPath);
+//   else console.log(newPath);
+// });
+
+// const fs = require("fs");
+
+// fs.unlink("./newJsFile.js", (err) => {
+//   if (err) throw err;
+// });
+
+// emitter.on("messegLogged", (...arg) => {
+//   console.log(arg);
+// });
+
+// emitter.emit("messegLogged", 1, 2, 3);
+
+// const Logger = require("./logger");
+
+// const logger = new Logger();
+
+// logger.on("hodisa1", (name) => {
+//   console.log(`My name is ${name}`);
+// });
+// logger.on("hodisa2", (name) => {
+//   console.log(`My name is ${name}`);
+// });
+// logger.on("hodisa3", (name) => {
+//   console.log(`My name is ${name}`);
+// });
+// logger.on("hodisa4", (name) => {
+//   console.log(`My name is ${name}`);
+// });
+
+// logger.goEvent();
---
+++ logger.js
@@ -0,0 +1,12 @@
+// const EventEmitter = require("node:events");
+
+// class Logger extends EventEmitter {
+//   goEvent() {
+//     this.emit("hodisa1", "Jalol");
+//     this.emit("hodisa2", "Ali");
+//     this.emit("hodisa3", "Nodir");
+//     this.emit("hodisa4", "Fozil");
+//   }
+// }
+
+// module.exports = Logger;
