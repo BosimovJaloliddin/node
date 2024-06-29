bfc7cfe9bf136d04fbd859529f1ca238b3d60d59
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-06-29 23:20:20 +0500

    practis

--- index.js
+++ index.js
@@ -100,81 +100,149 @@
 //   console.log(port);
 // });
 
-const books = [
-  { id: 1, name: "halqa" },
-  { id: 2, name: "halqa" },
-  { id: 3, name: "halqa" },
-  { id: 4, name: "halqa" },
-];
+// const books = [
+//   { id: 1, name: "halqa" },
+//   { id: 2, name: "halqa" },
+//   { id: 3, name: "halqa" },
+//   { id: 4, name: "halqa" },
+// ];
+
+// const express = require("express");
+// const Joi = require("joi");
+// const app = express();
+// app.use(express.json());
+
+// app.post("/api/books", (req, res) => {
+//   validCode(req, res);
+
+//   const book = {
+//     id: books.length + 1,
+//     name: req.body.name,
+//   };
+//   books.push(book);
+//   res.status(201).send(book);
+// });
+
+// app.put("/api/books/:id", (req, res) => {
+//   //Kitobni topib olamiz
+//   //Kitob bo'lmasa 404 qaytaramiz
+//   const book = books.find((val) => val.id === parseInt(req.params.id));
+//   if (!book) {
+//     return res.status(404).send("Kitob mavjud emas");
+//   }
+//   //kitob topilsa uni validatsiya qilamiz
+//   //agar kitob validatsiyadan o'tmasa 400 qayataramiz
+//   validCode(req, res);
+
+//   // kitobni yangilaymiz
+//   book.name = req.body.name;
+//   //yangilangan kitobni qaytaramiz
+//   res.send(book);
+// });
+
+// app.get("/api/books/:id", (req, res) => {
+//   const oneBook = books.find((val) => val.id === parseInt(req.params.id));
+//   if (!oneBook) {
+//     res.status(404).send("Kitob mavjud emas");
+//   }
+//   res.send(oneBook);
+// });
+// app.delete("/api/books/:id", (req, res) => {
+//   const book = books.find((val) => val.id === parseInt(req.params.id));
+//   if (!book) {
+//     return res.status(404).send("Kitob topilmadi");
+//   }
+//   const indexBook = books.indexOf(book);
+
+//   books.splice(indexBook, 1);
+
+//   res.send(book);
+// });
+
+// app.get("/books", (req, res) => {
+//   res.send(books);
+// });
+
+// function validCode(req, res) {
+//   const bookSchema = Joi.object({
+//     name: Joi.string().required().min(3),
+//   });
+
+//   const result = bookSchema.validate(req.body);
+
+//   if (result.error) {
+//     return res.status(400).send(result.error.details[0].message);
+//   }
+// }
+
+// app.listen(5000, () => {
+//   console.log(`5000 run port`);
+// });
 
 const express = require("express");
 const Joi = require("joi");
 const app = express();
 app.use(express.json());
 
-app.post("/api/books", (req, res) => {
-  validCode(req, res);
+const cars = [
+  { id: 1, name: "cobalt", year: 2024 },
+  { id: 2, name: "nexia", year: 2024 },
+  { id: 3, name: "bmw", year: 2024 },
+  { id: 4, name: "kaptiva", year: 2024 },
+];
 
-  const book = {
-    id: books.length + 1,
+app.get("/api/cars", (req, res) => {
+  res.send(cars);
+});
+app.post("/api/cars", (req, res) => {
+  validetionFun(res, req);
+
+  const car = {
+    id: cars.length + 1,
     name: req.body.name,
+    year: req.body.year,
   };
-  books.push(book);
-  res.status(201).send(book);
+  cars.push(car);
+  res.status(201).send(car);
 });
 
-app.put("/api/books/:id", (req, res) => {
-  //Kitobni topib olamiz
-  //Kitob bo'lmasa 404 qaytaramiz
-  const book = books.find((val) => val.id === parseInt(req.params.id));
-  if (!book) {
-    return res.status(404).send("Kitob mavjud emas");
+app.put("/api/cars/:id", (req, res) => {
+  const car = cars.find((v) => v.id === parseInt(req.params.id));
+  if (!car) {
+    return res.send("Bu id'da ma'lumot mavjud emas");
   }
-  //kitob topilsa uni validatsiya qilamiz
-  //agar kitob validatsiyadan o'tmasa 400 qayataramiz
-  validCode(req, res);
 
-  // kitobni yangilaymiz
-  book.name = req.body.name;
-  //yangilangan kitobni qaytaramiz
-  res.send(book);
+  validetionFun(res, req);
+
+  car.name = req.body.name;
+  car.yera = req.body.yera;
+
+  res.send(car);
 });
-
-app.get("/api/books/:id", (req, res) => {
-  const oneBook = books.find((val) => val.id === parseInt(req.params.id));
-  if (!oneBook) {
-    res.status(404).send("Kitob mavjud emas");
+app.delete("/api/cars/:id", (req, res) => {
+  const car = cars.find((v) => v.id === parseInt(req.params.id));
+  if (!car) {
+    return res.send("Bu id'da ma'lumot yo'q");
   }
-  res.send(oneBook);
-});
-app.delete("/api/books/:id", (req, res) => {
-  const book = books.find((val) => val.id === parseInt(req.params.id));
-  if (!book) {
-    return res.status(404).send("Kitob topilmadi");
-  }
-  const indexBook = books.indexOf(book);
 
-  books.splice(indexBook, 1);
-
-  res.send(book);
+  const indexCar = cars.indexOf(car);
+  cars.splice(indexCar, 1);
+  res.status(200).send(cars);
 });
 
-app.get("/books", (req, res) => {
-  res.send(books);
-});
-
-function validCode(req, res) {
-  const bookSchema = Joi.object({
-    name: Joi.string().required().min(3),
+function validetionFun(res, req) {
+  const carSchema = Joi.object({
+    name: Joi.string().required().min(1),
+    year: Joi.number().required(),
   });
 
-  const result = bookSchema.validate(req.body);
+  const result = carSchema.validate(req.body);
 
   if (result.error) {
-    return res.status(400).send(result.error.details[0].message);
+    return res.status(404).send(result.error.details);
   }
 }
 
-app.listen(5000, () => {
-  console.log(`5000 run port`);
+app.listen(5001, () => {
+  console.log("5001 run port");
 });
